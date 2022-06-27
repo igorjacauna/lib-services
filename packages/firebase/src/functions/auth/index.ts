d9c8e04fe80b7f3975e51b8b83b172c802d53433
Igor Jacaúna <igor.jacauna@meliuz.com.br>
2022-06-27 19:58:22 -0400

    feat(firebase): Adiciona functions

---
+++ packages/firebase/src/functions/auth/index.ts
@@ -0,0 +1,15 @@
+import * as functions from "firebase-functions";
+import {firestore} from "firebase-admin";
+
+const createUser = functions.auth.user().onCreate((user) => {
+  const db = firestore();
+  const {uid} = user;
+
+  const userCollection = db.collection("users");
+
+  userCollection.doc(uid).set({});
+});
+
+export default {
+  createUser,
+};
--- packages/firebase/src/index.ts
+++ packages/firebase/src/index.ts
@@ -1,4 +1,5 @@
 export * from './firebase';
 export * from './auth';
 export * from './composables';
+export * as functions from './functions';
 
