9616f02275ef0c59353a8d7458f5088a1bed04ba
Igor Jacaúna <igor.jacauna@meliuz.com.br>
2022-07-01 16:14:41 -0400

    fix: Reverte alteração

--- packages/lib-firebase/src/functions/auth/index.ts
+++ packages/lib-firebase/src/functions/auth/index.ts
@@ -7,7 +7,7 @@ const createUser = functions.auth.user().onCreate((user) => {
 
   const userCollection = db.collection("users");
 
-  userCollection.doc(uid).set(user.toJSON());
+  userCollection.doc(uid).set({});
 });
 
 export default {
